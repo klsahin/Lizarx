f2668c5e0bb568bf8b3326bde15d3a043e375568
klsahin <klsahin@andrew.cmu.edu>
2025-07-01 17:08:52 -0400

    turning all directions with keys

--- classes.py
+++ classes.py
@@ -1,11 +1,22 @@
 import pygame
 
-class Background:
-    def __init__(self, width, height, path1 = "assets/background1", path2 = "assets/background2"):
+class Leaves:
+    def __init__(self, width, height, path = "assets/leaves.png"):
+        self.position = [0,0]
+        self.size = [width, height]
+        self.imagePath = path
+    
+    def draw(self, screen):
+        self.image = pygame.image.load(self.imagePath).convert_alpha()
+        self.image = pygame.transform.scale(self.image, self.size)
+        screen.blit(self.image, self.position)
+
+class Tree:
+    def __init__(self, width, height, path1 = "assets/tree1.png"):#, path2 = "assets/tree2.png"):
         self.position = [0, 0]
         self.size = [width, height]
         self.imagePath1 = path1
-        self.imagePath2 = path2
+        #self.imagePath2 = path2
         self.image1 = None
         self.image2 = None
         self.y_offset = 0
@@ -13,18 +24,18 @@ class Background:
     def load_image(self):
         self.image1 = pygame.image.load(self.imagePath1).convert_alpha()
         self.image1 = pygame.transform.scale(self.image1, self.size)
-        self.image2 = pygame.image.load(self.imagePath2).convert_alpha()
-        self.image2 = pygame.transform.scale(self.image2, self.size)
+        #self.image2 = pygame.image.load(self.imagePath2).convert_alpha()
+        #self.image2 = pygame.transform.scale(self.image2, self.size)
 
     def draw(self, screen):
         # Draw two backgrounds side by side for endless effect
         w = self.size[1]
-        y = -self.y_offset % w
+        y = -self.y_offset % w #scrolls upwards
         screen.blit(self.image1, (0, y))
-        screen.blit(self.image2, (0, y - w))
+        #screen.blit(self.image2, (0, y - w))
         # If x > 0, draw the second background before the first
-        if y > 0:
-            screen.blit(self.image2, (0, y + w))
+        #if y > 0:
+            #screen.blit(self.image2, (0, y + w))
 
     def scroll(self, dy):
         self.y_offset += dy
@@ -38,65 +49,98 @@ class Lizard:
         self.position = [self.x, self.y]
         self.size = [width, height]
         self.index = index
-        self.path = f'assets/lizard{self.index+1}.png' #based on numbering system
+        self.flip = False # left
+        self.path = f'assets/straight/S1.png' #based on numbering system
         self.image = None  # Placeholder for the image, to be loaded later
 
     def load_image(self):
         self.image = pygame.image.load(self.path).convert_alpha()
         self.image = pygame.transform.scale(self.image, self.size)
+        self.image = pygame.transform.flip(self.image, self.flip, False)
     
-    def run(self):
+    def run(self, screen, leaves, tree):
         #straight movement
-        pass
+        for frame in range(1,7):
+            dy = 10
+            leaves.draw(screen)
+            # tree.scroll(dy)
+            tree.draw(screen)
+            self.path = f'assets/straight/S{frame}.png'
+            self.load_image()
+            screen.blit(self.image, self.position)
+            pygame.display.flip()
+            pygame.time.wait(100)
+
+        
     
-    def turn(self, farLeft, topLeft, topRight, farRight, screen, background):
+    def turn(self, farLeft, topLeft, topRight, farRight, screen, leaves, tree):
         if farLeft and topLeft:
             print("Turning far left")
             #animation
-            turn_frames = []
+            direction = "farLeft/FL"
             #position change
             dx = - 10
             dy = 0
+            self.flip = False
             
 
         elif farLeft or topLeft: # xor: small turn
             print("Turning top left")
             #animation
-            turn_frames = []
+            direction = "topLeft/TL"
             #position change
             dx = - 5
             dy = 5
+            self.flip = False
+            
+            
             
         elif topRight and farRight:
             print("Turning far right")
             #animation
-            turn_frames = []
+            direction = "farLeft/FL"
             #position change
             dx = 10
             dy = 0
+            self.flip = True
 
         elif topRight or farRight: # xor: small turn
             print("Turning top right")
             #animation
-            turn_frames = []
+            direction = "topLeft/TL"
             #position change
             dx = 5
             dy = 5
+            self.flip = True
 
-        for frame in turn_frames:
+        else: # running straight
+            print("Running straight")
+            #animation
+            direction = "straight/S"
+            #position change
+            dx = 0
+            dy = 10
+            self.flip = False
+
+        #self.size = [scale * d for d in self.size]
+        for frame in range(1,7):
             #redraw background
-            background.scroll(dy)
-            background.draw(screen)
+            leaves.draw(screen)
+            #tree.scroll(dy)
+            tree.draw(screen)
 
             #update lizard position and image
-            self.path = f'assets/lizard{frame}.png'
+            self.path = f'assets/{direction}{frame}.png'
             self.position[0] += dx
-            self.position[1] += dy
+            
+            
+            #self.position[1] += dy
 
             self.load_image()
             screen.blit(self.image, self.position)
             pygame.display.flip()
             pygame.time.delay(100)  # Delay to control the speed of the turn
-            
+
+           
 
 
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame
 from classes import *
 import random
-
-
+arduino = False
+'''
 # ARDUINO SETUP
 import serial
 import serial.tools.list_ports
 import time
 import csv
 
-arduino = False
+
 
 if arduino:
     # Identify the correct port
@@ -36,22 +36,28 @@ if arduino:
     dataVariable = 0
 
 
+'''
 
-
+screenWidth = 700
+screenHeight = 800
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 running = True
 
 
 # Load Object
-background = Background(800, 600)
-lizard = Lizard(400, 400, 50, 50, 0)  # Example parameters for lizard
+leaves = Leaves(screenWidth, screenHeight)
+tree = Tree(screenWidth,screenHeight)
+tree.load_image()
+lizard = Lizard(300, 500, 200, 300, 1)  # Example parameters for lizard
+lizard.load_image()
 
 # draw all objects
 def draw_objects():
     #draw background
-    background.draw(screen)
+    leaves.draw(screen)
+    tree.draw(screen)
     #draw lizard
     screen.blit(lizard.image, lizard.position)
 
@@ -60,33 +66,51 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            lizard.turn(False, False, False, False, screen, leaves, tree)
+        elif keys[pygame.K_u]:
+            lizard.turn(False, True, False, False, screen, leaves, tree)
+        elif keys[pygame.K_h]:
+            lizard.turn(True, True, False, False, screen, leaves, tree)
+        elif keys[pygame.K_i]:
+            lizard.turn(False, False, True, False, screen, leaves, tree)
+        elif keys[pygame.K_l]:
+            lizard.turn(False, False, True, True, screen, leaves, tree)
 
 
+    draw_objects()  # Call the function to draw objects
+    pygame.display.flip()  # Update the display
+
+""" 
     try:
+        
         if arduino:
-            serialCom.flushInput()  # Clear the input buffer
-            s_bytes = serialCom.readline()
-            decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
-            print(f"decoded bytes: {decoded_bytes}")
-            farLeftData = []
-            topLeftData = []
-            topRightData = []
-            farRightData = []
-            count = 0
+            pass
+            # serialCom.flushInput()  # Clear the input buffer
+            # s_bytes = serialCom.readline()
+            # decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
+            # print(f"decoded bytes: {decoded_bytes}")
+            # farLeftData = []
+            # topLeftData = []
+            # topRightData = []
+            # farRightData = []
+            # count = 0
 
-            # Split the data by commas and store in leftData and rightData
-            for data in decoded_bytes.split(","):
-                if count < 2:
-                    farLeftData.append(data)
-                elif count < 4:
-                    topLeftData.append(data)
-                elif count < 6:
-                    topRightData.append(data)
-                elif count < 8:
-                    farRightData.append(data)
+            # # Split the data by commas and store in leftData and rightData
+            # for data in decoded_bytes.split(","):
+            #     if count < 2:
+            #         farLeftData.append(data)
+            #     elif count < 4:
+            #         topLeftData.append(data)
+            #     elif count < 6:
+            #         topRightData.append(data)
+            #     elif count < 8:
+            #         farRightData.append(data)
 
-                count += 1
+            #     count += 1
         else: # Simulate data with. H, U, I, L keys
+            complete = False
             for key in pygame.key.get_pressed():
                 if key == pygame.K_h:
                     farLeftData = [2000,2000]
@@ -96,11 +120,12 @@ while running:
                     topRightData = [2000,2000]
                 elif key == pygame.K_l:
                     farRightData = [2000,2000]
-                else:
-                    farLeftData = [0, 0]
-                    topLeftData = [0, 0]
-                    topRightData = [0, 0]
-                    farRightData = [0, 0]
+                complete = True
+            if not complete:
+                farLeftData = [0, 0]
+                topLeftData = [0, 0]
+                topRightData = [0, 0]
+                farRightData = [0, 0]
         
         farLeftTurn = False
         topLeftTurn = False
@@ -128,11 +153,8 @@ while running:
         print("Error reading data from serial port")
         farLeftTurn = topLeftTurn = topRightTurn = farRightTurn = False
         
-
-    draw_objects()  # Call the function to draw objects
-    pygame.display.flip()  # Update the display
-
-
+     """
+    
 
 pygame.quit()
 f.close()  # Close the CSV file
